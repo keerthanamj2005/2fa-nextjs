21253e225b4c6b105fe95b2a66e9aeaf58e39a68
keerthana m j <146466521+keerthanamj2005@users.noreply.github.com>
2023-10-14 23:43:15 +0000

    Update 2 files

--- components/User.tsx
+++ components/User.tsx
@@ -25,12 +25,15 @@ export function UserComponent({ currentUser }: Props) {
   }
 
   return (
-    <div className="bg-white h-screen w-screen">
-      <div className="flex flex-col justify-center items-center px-12 gap-y-12 pt-40">
+    <div className="bg-white h-screen w-screen overflow-y-scroll">
+      <div className="flex flex-col justify-center items-center px-12 gap-y-12 pt-5">
         <h2 className="mt-20 text-3xl text-center font-bold text-gray-800">
-          Welcome to CyberSecurity Hackathon 2023. <br />
-          <span className="">Jyothy Institute of Technology.</span>
+          Welcome to CyberSecurity Hackathon 2023.
         </h2>
+        <span className="text-lg font-bold -mt-[25px]">
+          Jyothy Institute of Technology.
+        </span>
+
         {currentUser &&
           currentUser.emailVerified &&
           !verifyIfUserIsEnrolled(currentUser) && (
@@ -58,6 +61,58 @@ export function UserComponent({ currentUser }: Props) {
             Disconnect
           </span>
         </button>
+
+        <hr />
+        <div className="w-full px-10">
+          <p className="text-xl">
+            In this report, I will walk you through the process of creating a
+            secure Two-Factor Authentication (2FA) website using Next.js,
+            Firebase, React Toast, Phosphor React, and TypeScript. 2FA is a
+            crucial security feature that enhances the protection of user
+            accounts and sensitive data. By combining these technologies, we've
+            built a user-friendly and secure platform.
+          </p>
+          <h1 className="text-3xl font-bold mt-8">Technologies Used</h1>
+
+          <div className="mt-10 px-3">
+            <span className="text-2xl font-bold mb-2">Next.js</span> <br />
+            Next.js is a React framework that simplifies server-side rendering
+            and provides an excellent development experience. We've leveraged it
+            for creating a responsive and fast web application.
+            <br />
+          </div>
+          <div className="mt-10 px-3">
+            <span className="text-2xl font-bold mb-2">Firebase</span> <br />
+            Firebase offers a suite of cloud-based tools for building web and
+            mobile applications. We've used Firebase for user authentication,
+            real-time database, and hosting to ensure our site is reliable and
+            secure.
+          </div>
+          <div className="mt-10 px-3">
+            <span className="text-2xl font-bold mb-2">React Toast</span> <br />
+            React Toast is a library for displaying user-friendly notifications.
+            We've implemented it to provide informative feedback to our users
+            during the 2FA setup and login process.
+          </div>
+          <div className="mt-10 px-3">
+            <span className="text-2xl font-bold mb-2">Phosphor Icons</span>{" "}
+            <br />
+            Phosphor Icons offers a beautiful set of open-source icons. We've
+            integrated Phosphor React to enhance the visual appeal of our site.
+          </div>
+          <div className="mt-10 px-3">
+            <span className="text-2xl font-bold mb-2">Typescript</span> <br />
+            TypeScript is a statically typed superset of JavaScript. We used it
+            to write clean, maintainable code and catch errors during
+            development.
+          </div>
+          <div className="mt-10 px-3">
+            <span className="text-2xl font-bold mb-2">React Toast</span> <br />
+            React Toast is a library for displaying user-friendly notifications.
+            We've implemented it to provide informative feedback to our users
+            during the 2FA setup and login process.
+          </div>
+        </div>
       </div>
     </div>
   );
